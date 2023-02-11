0735d270f729074f092f8999695477bf5a9e703b
YoungSang Jung <mikey5843@naver.com>
2023-02-12 07:42:23 +0900

    calmdownman final

--- static/js/login.js
+++ static/js/login.js
@@ -1,39 +1,70 @@
-$(document).ready(function () {});
+// $(document).ready(function () {});
+//
+// function login() {
+//   $.ajax({
+//     type: "POST",
+//     url: "/login",
+//     data: {
+//       id_give: $("#login_id").val(),
+//       pw_give: $("#login_pw").val(),
+//     },
+//     success: function (response) {
+//       alert(response["msg"]);
+//       window.location.href = "/index";
+//     },g
+//   });
+// }
+// function login() {
+//   $.ajax({
+//     type: "POST",
+//     url: "/login",
+//     data: {
+//       id_give: $("#login_id").val(),
+//       pw_give: $("#login_pw").val(),
+//     },
+//
+//     success: function (response) {
+//       if (response["result"] == "로그인 성공!") {
+//         alert(response["result"]);
+//         window.location.href = "/index";
+//       } else {
+//         alert(response["result"]);
+//       }
+//     },
+//   });
+// }
 
-function login() {
-  $.ajax({
-    type: "POST",
-    url: "/login",
-    data: {
-      id_give: $("#login_id").val(),
-      pw_give: $("#login_pw").val(),
-    },
-    success: function (response) {
-      alert(response["msg"]);
-      window.location.href = "/index";
-    },
-  });
+function signup() {
+  window.location.href = "/signup";
 }
+
+$(document).ready(function () {
+
+});
+
 function login() {
   $.ajax({
     type: "POST",
-    url: "/login",
+    url: "/api/login",
     data: {
-      id_give: $("#login_id").val(),
-      pw_give: $("#login_pw").val(),
+      id_give: $('#login_id').val(),
+      pw_give: $('#login_pw').val()
     },
-
     success: function (response) {
-      if (response["result"] == "로그인 성공!") {
-        alert(response["result"]);
-        window.location.href = "/index";
+      if (response['result'] == 'success') {
+        console.log(response)
+        $.cookie('mytoken', response['token']);
+        alert("로그인 되었습니다")
+        window.location.href = '/index'
       } else {
-        alert(response["result"]);
+        alert(response['result'])
       }
-    },
+
+
+    }
   });
 }
 
 function signup() {
-  window.location.href = "/signup";
+  window.location.href = '/signup'
 }
